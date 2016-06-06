6f57d5bdceeff7327dd5528e943fa9af9e07e39f
Tim Lucas <t@toolmantim.com>
2016-06-07 08:27:57 +1000

    Get :package:'ing working

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var webpack = require('webpack');
+
 module.exports = {
   entry: "./entry.js",
   output: {
@@ -21,5 +23,12 @@ module.exports = {
         ]
       }
    ]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    })
+  ]
 }
